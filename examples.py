92a34fdffc6a2862eaaa3cf7094d579ebe8038ad
wlong007 <wlong007@gmail.com>
2015-07-23 11:39:29 -0500

    Update examples.py

--- examples.py
+++ examples.py
@@ -21,22 +21,22 @@ import matheval
 # initialize new mathevel object
 m=matheval.matheval()
 # basic evaluation
-result=m.evaluate('2+2')
+result=m.evaluate('2+2') # returns 4
 print(result)
 # supports: order of operation, parenthese, negation, and built-in functions
 result=m.evaluate('-8(5/2)^2*(1-sqrt(4))-8')
-print(result)
+print(result) # returns 42
 # create your own variables and use built-in constants
 result=m.evaluate('a = e^(ln(pi))')
-print(result)
+print(result) # returns 3.141592653589793
 # create your own functions
 result=m.evaluate('f(x,y) = x^2 + y^2 - 2x*y + 1')
 # and then use them
 result=m.evaluate('3*f(42,a)')
-print(result)
+print(result) # returns 4532.92746449864
 # get all user-defined variables
 result=m.vars()
-print(result)
+print(result) # returns {'a': 3.141592653589793}
 # get all user-defined functions
 result=m.funcs()
-print(result)
+print(result) # returns ['f(x,y)']
